b9285319e135244bce8f254241d6a5e2a694a046
Henry J Schmale <hschmale16@gmail.com>
2015-04-25 21:07:45 -0400

    Benchmark script works

--- test.pl
+++ test.pl
@@ -1,9 +1,30 @@
 #!/usr/bin/perl
+$ITER = 50; # Iterations for the benchmark
 
 # build prog
-system("sh -c make");
+system("sh -c make > /dev/null");
 
 # benchmark pthread
-for($a = 0; $a < 1000; $a++) {
-
+$pdt = 0; # pthread delta t
+for($a = 0; $a < $ITER; $a++) {
+    system("{ time ./pthread > /dev/null ; } 2> junk");
+    my $out = `tail -1 junk`;
+    $out=~s/[^.0-9]//g;
+    $pdt += $out;
 }
+$pdt = $pdt / $ITER;
+
+# bench bthread
+$bdt = 0; # bthread delta t
+for($a = 0; $a < $ITER; $a++) {
+    system("{ time ./bthread > /dev/null ; } 2> junk");
+    my $out = `tail -1 junk`;
+    $out=~s/[^.0-9]//g;
+    $bdt += $out;
+}
+$bdt = $bdt / $ITER;
+
+# output the results whatever they may be
+printf("pthread avg runtime = %f\n", $pdt);
+printf("boost thread avg    = %f\n", $bdt);
+
