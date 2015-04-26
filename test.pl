809870b745cff0859c510aeaf729fe23acdb61a0
Henry J Schmale <hschmale16@gmail.com>
2015-04-26 15:24:15 -0400

    Improoved Test Script
    * More tests
    * avg time & total time for usr, sys, real as output by bash cmd: `time`
    * outputs the results of the test
    * output final winner

--- test.pl
+++ test.pl
@@ -1,14 +1,38 @@
 #!/usr/bin/perl
-$ITER = 50.0; # Iterations for the benchmark
+$ITER = 1000.0; # Iterations for the benchmark
 
 # build prog
 system("sh -c make > /dev/null");
 
-Bench("pthread", $ITER);
-Bench("bthread", $ITER);
+@p = Bench("pthread", $ITER);
+@b = Bench("bthread", $ITER);
+
+# determine winner
+print "\n ---- Results ----\n";
+$pwin = 0; $bwin = 0;
+for($a = 0; $a < scalar(@p); $a++){
+    # low score wins
+    if($p[$a] < $b[$a]){
+        $pwin += 1;
+        $w = "pthreads";
+    }else{
+        $bwin += 1;
+        $w = "boost";
+    }
+    printf("Test %d Winner: %s\n", $a, $w);
+}
+
+# the final winner is who had the most points at the end
+if($pwin > $bwin){
+    $w = "pthreads";
+}else{
+    $w = "boost";
+}
+printf("\nFinal Winner: %s\n", $w);
+
 
 # Perform Clean-up
-# system("rm junk");
+system("rm junk");
 system("sh -c make clean > /dev/null");
 
 # ---- Subroutines ----
@@ -37,14 +61,16 @@ sub Bench{
         $dt_sys += $out;
     }
     print  "\n  --- $_[0] ---\n";
-    printf("Bin Size = %d\n\n", $sz);
-    printf("tot Real = %f\n", $dt_rea);    
-    printf("tot User = %f\n", $dt_usr);
-    printf("tot Sys  = %f\n", $dt_sys);
-    $dt_rea /= $ITER;
-    $dt_usr /= $ITER;
-    $dt_sts /= $ITER;
-    printf("avg Real = %f\n", $dt_rea);
-    printf("avg User = %f\n", $dt_usr);
-    printf("avg Sys  = %f\n", $dt_sys);
+    printf("Bin Size = %d bytes\n\n", $sz);
+    printf("tot Real = %f sec\n", $dt_rea);    
+    printf("tot User = %f sec\n", $dt_usr);
+    printf("tot Sys  = %f sec\n\n", $dt_sys);
+    local $at_rea = $dt_rea / $ITER;
+    local $at_usr = $dt_usr / $ITER;
+    local $at_sys = $dt_sys / $ITER;
+    printf("avg Real = %f sec\n", $at_rea);
+    printf("avg User = %f sec\n", $at_usr);
+    printf("avg Sys  = %f sec\n", $at_sys);
+    @res = ($sz, $dt_sys, $dt_usr, $dt_rea, $at_sys, $at_usr, $at_rea);
+    return @res;
 }
